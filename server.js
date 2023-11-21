0512249ebee9feaecaf9e93c02f93b9f24e6b6df
FakeGamer31 <yusuf.97@hotmail.de>
2023-11-21 21:01:20 +0100

    create exe, changed some stuff

--- server.js
+++ server.js
@@ -3,42 +3,59 @@ const http = require("http");
 const socketIO = require("socket.io");
 const chokidar = require("chokidar");
 const fs = require("fs");
+const readline = require("readline");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server);
 
-const filePath = "F:/Emulator/Project64/test.json";
+let filePath = "";
+
+function askForFilePath() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    rl.question("Geben Sie den Dateipfad ein: ", (answer) => {
+      filePath = answer;
+      rl.close();
+      resolve();
+    });
+  });
+}
 
 app.use(express.static(__dirname));
-
-// Überwache die Datei auf Änderungen
-const watcher = chokidar.watch(filePath);
-watcher.on("change", () => {
-  fs.readFile(filePath, "utf-8", (err, data) => {
-    if (err) {
-      console.error(err);
-      return;
-    }
-    try {
-      const jsonData = JSON.parse(data);
-      io.emit("json-update", jsonData);
-    } catch (error) {
-      console.error("Error with Json ", error);
-    }
+askForFilePath().then(() => {
+  // Überwache die Datei auf Änderungen
+  const watcher = chokidar.watch(filePath);
+  watcher.on("change", () => {
+    fs.readFile(filePath, "utf-8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      try {
+        const jsonData = JSON.parse(data);
+        io.emit("json-update", jsonData);
+      } catch (error) {
+        console.error("Error with Json ", error);
+      }
+    });
   });
-});
 
-debugger;
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+  });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+  io.on("connection", (socket) => {
+    // Hier können Sie bei Bedarf weitere Socket.IO-Logik hinzufügen
+  });
 
-io.on("connection", (socket) => {
-  // Hier können Sie bei Bedarf weitere Socket.IO-Logik hinzufügen
-});
-
-server.listen(3000, () => {
-  console.log("Server is running on http://localhost:3000");
+  server.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+    console.log("Filepath of json-File is", filePath);
+    console.log("res.sendFile: ", __dirname + "/index.html");
+  });
 });
